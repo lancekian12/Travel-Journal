0423b8f72543d10f43e1abefa6cf63734301711e
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-06 20:16:43 +0800

    created the header and added inter font

---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <img src="/src/assets/images/Globe.png" alt="globe icon" />
+            <h1>my travel journal.</h1>
+        </header>
+    )
+}
+
+export default Header
