d4d2ad21fcfc10f633f99b6cd1ad120826b019fb
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-10 00:51:58 +0800

    applied the knowledge of props into the travel journey

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Entry from './component/Entry'
 import Header from "./component/Header"
-import Joke from './component/Joke'
+// import Joke from './component/Joke'
 
 
 function App() {
@@ -9,9 +9,16 @@ function App() {
   return (
     <>
       <Header />
-      <Entry />
+      <Entry img={{ src: "https://scrimba.com/links/travel-journal-japan-image-url", alt: "Mount Fuji" }}
+        title="Mount Fuji"
+        country="Japan"
+        googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+        dates="12 Jan, 2021 - 24 Jan, 2021"
+        text=" Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+
+      />
       <hr />
-      <Joke
+      {/* <JokeF
         punchline="It's hard to explain puns to kleptomaniacs because they always take things literally."
         upVotes={10}
         isPun={true}
@@ -39,7 +46,7 @@ function App() {
         setup="What's the best thing about Switzerland?"
         punchline="I don't know, but the flag is a big plus!"
         isPun={false}
-      />
+      /> */}
     </>
   )
 }
--- src/component/Joke.jsx
+++ src/component/Joke.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+// import React from 'react'
 
-const Joke = (props) => {
-    return (
-        <>
-            {props.setup && <p className='setup'>Setup: {props.setup}</p>}
-            {<p style={{ display: props.setup ? "block" : "none" }} className='punchline'>{props.punchline}</p>}
-            {props.setup && props.punchline && <hr />}
-            {props.upVotes && <p>Upvotes: {props.upVotes}</p>}
+// const Joke = (props) => {
+//     return (
+//         <>
+//             {props.setup && <p className='setup'>Setup: {props.setup}</p>}
+//             {<p style={{ display: props.setup ? "block" : "none" }} className='punchline'>{props.punchline}</p>}
+//             {props.setup && props.punchline && <hr />}
+//             {props.upVotes && <p>Upvotes: {props.upVotes}</p>}
 
-        </>
-    )
-}
+//         </>
+//     )
+// }
 
-export default Joke
+// export default Joke
