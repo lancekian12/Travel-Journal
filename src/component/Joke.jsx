7ad2e6249fda5c64d9618d5c34a8ed20cd4be4c6
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-16 22:41:17 +0800

    re-learned about ternary operator

--- src/component/Joke.jsx
+++ src/component/Joke.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Joke = (props) => {
     const [isShown, setIsShown] = React.useState(false)
-    const [unreadMessages] = React.useState(["ab"])
+    // const [unreadMessages] = React.useState(["ab"])
 
 
     function toggleShown() {
@@ -13,12 +13,12 @@ const Joke = (props) => {
     return (
         <div>
             {props.setup && <h3>{props.setup}</h3>}
-            {isShown && <p>{props.punchline}</p>}
-            <button onClick={toggleShown}>Show punchline</button>
+            {isShown ? <p>{props.punchline}</p> : null}
+            <button onClick={toggleShown}>{isShown ? "Hide" : "Show"} punchline</button>
             <hr />
-            <div>
+            {/* <div>
                 {unreadMessages.length > 0 && <h1>You have _ unread messages!</h1>}
-            </div>
+            </div> */}
         </div>
 
     )
